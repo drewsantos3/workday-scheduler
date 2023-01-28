a9e0ff79b090b60fcd3321bd76e8ef26ef33b714
Drew S <andrewsantos2022@u.northwestern.edu>
2023-01-27 19:51:17 -0600

    added event listener for save btn

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  let buttons = $(".saveBtn");
+
+  buttons.on("click", function (event) {
+    let parent = $(this).parent().attr("id");
+    let description = $(this).siblings(".description").val();
+    console.log(parent, description);
+    localStorage.setItem(parent, description);
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
